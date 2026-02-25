9e092957766d686940a16ab432832e7fb2ed86cf
ISSKK <isskk-yt@outlook.com>
2026-02-25 00:31:31 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@ sleep 1
 
 cd /home/container
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+# Crear directorios necesarios (por si acaso)
+mkdir -p /home/container/tmp /home/container/logs /home/container/nginx/conf.d
+
+# Reemplazar variables en el comando de inicio
+MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
-${MODIFIED_STARTUP}
+# Ejecutar el servidor
+exec ${MODIFIED_STARTUP}
